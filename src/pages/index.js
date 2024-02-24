58f73905e7940b8e7e8c79a494d4546be1bea09a
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2024-02-24 19:08:09 +0100

    8th commit!

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
         <img src="/img/logo_docs_light.png" draggable="false" className='h-72 opacity-90 dark:hidden max-md:h-32'></img>
         <hr className='w-2/3 bg-neutral-700 h-1 mt-3'></hr>
         <div className='flex gap-8 mt-1 items-center justify-center max-md:flex-col max-md:gap-4 max-md:mb-4'>
-            <a href='/docs/current/docs/get-started' className='bg-[#f8ba00] text-white text-lg font-bold py-2 px-8 rounded-lg hover:bg-white hover:text-black hover:no-underline'>Get Started!</a>
-            <a href='/docs/current/developer/get-started' className='bg-neutral-700 text-white text-lg font-bold py-2 px-8 rounded-lg hover:bg-white hover:text-black hover:no-underline'>Developer</a>
-            <a href='/docs/current/plugin/get-started' className='bg-neutral-700 text-white text-lg font-bold py-2 px-8 rounded-lg hover:bg-white hover:text-black hover:no-underline'>Plugins</a>
+            <a href='/docs/dev/docs/get-started' className='bg-[#f8ba00] text-white text-lg font-bold py-2 px-8 rounded-lg hover:bg-white hover:text-black hover:no-underline'>Get Started!</a>
+            <a href='/docs/dev/developer/get-started' className='bg-neutral-700 text-white text-lg font-bold py-2 px-8 rounded-lg hover:bg-white hover:text-black hover:no-underline'>Developer</a>
+            <a href='/docs/dev/plugin/get-started' className='bg-neutral-700 text-white text-lg font-bold py-2 px-8 rounded-lg hover:bg-white hover:text-black hover:no-underline'>Plugins</a>
             <a href='/blog' className='bg-neutral-700 text-white text-lg font-bold py-2 px-8 rounded-lg hover:bg-white hover:text-black hover:no-underline'>Blog</a>
         </div>
         <p className='my-2 text-lg font-bold'>OR</p>

263914544ca16e51aa80537c2e04bbc4fe233646
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2025-01-31 17:39:20 +0100

    Synced homepage to Open Ticket README.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,23 +30,34 @@ export default function Home() {
             </div>
             <ul>
                 <h2 className='text-4xl -ml-4'>Open Ticket Features</h2>
-                <li><b>💩 no bloat/credits</b> - Your Open Ticket bot doesn't contain any form of bloat or credits!</li>
-                <li><b>📈 scalable</b> - Open Ticket is made to handle huge servers! Even if your server contains more than 100k members!</li>
-                <li><b>📄 html transcripts</b> - Make use of the most customisable, beautiful and easy-to-use HTML Transcripts!</li>
+                <li><b>💩 no bloat</b> - Your Open Ticket bot doesn't contain any form of bloat or credits!</li>
+                <li><b>🔒 tested & secure</b> - Open Ticket has been tested by thousands of servers and is very secure!</li>
+                <li><b>📈 scalable</b> - Open Ticket is made to handle huge servers! (Already tested in <b>servers with 100k members</b>)</li>
+                <li><b>📄 HTML transcripts</b> - Make use of the most customisable, beautiful and easy-to-use HTML Transcripts!</li>
                 <li><b>✅ ticket actions</b> - Close, Reopen, Delete, Rename & Move all your tickets!</li>
-                <li><b>🇬🇧 translation</b> - Open Ticket has been translated in more than 16 languages by our community!</li>
+                <li><b>🇬🇧 translation</b> - Open Ticket has been translated in more than <b>22 languages</b> by our community!</li>
                 <li><b>🎨 customisation</b> - Open Ticket has been created around customisation, everything can be customised!</li>
                 <li><b>🖥️ interactions</b> - The bot has full support for Buttons, Dropdowns, Slash Commands and Modals!</li>
-                <li><b>∞ unlimited everywhere</b> - Create an infinite amount of tickets & panels!</li>
-                <li><b>📝 advanced plugins</b> - Create the most advanced plugins that you want or use premade ones by our community!</li>
+                <li><b>∞ unlimited</b> - Create an infinite amount of tickets & panels!</li>
+                <li><b>📝 advanced plugins</b> - Create advanced plugins or use pre-made ones by our community!</li>
                 <li><b>👥 user management</b> - Add & Remove users from all tickets!</li>
                 <li><b>📊 detailed stats</b> - Open Ticket has ticket, user & global staticstics available for everyone!</li>
                 <li><b>🚫 blacklist</b> - Blacklist users to prevent them from creating a ticket!</li>
                 <li><b>❓ questions</b> - Let users answer questions in a modal before the ticket is created!</li>
-                <li><b>📦 commands</b> - Open Ticket supports both slash & text commands!</li>
-                <li><b>📥 extra types</b> - The bot also supports Reaction Roles & Url Buttons, because why not ¯\(ツ)/¯</li>
-                <li><b>🎛️ dependencies</b> - We try to use as little external dependencies as needed!</li>
-                <li><b>📢 commands</b> - The bot contains more than 25 commands! And it's expandable with plugins!</li>
+                <li><b>📦 slash & text</b> - Open Ticket supports both slash & text commands!</li>
+                <li><b>📥 extra buttons</b> - The bot also supports Reaction Roles & Url Buttons, because why not ¯\\_(ツ)_/¯</li>
+                <li><b>✨ commands</b> - The bot contains more than 25 commands!</li>
+                <br></br>
+                <h3>And more using plugins!</h3>
+                <li><b>💬 reviews</b> - Create & customise your own review system!</li>
+                <li><b>📢 feedback</b> - Collect feedback & create forms for people to answer!</li>
+                <li><b>🏷️ tags</b> - Create tags & answer questions automatically using keywords!</li>
+                <li><b>📝 forms</b> - Create advanced forms and ask people for additional details!</li>
+                <li><b>🔄 rotating status</b> - Create a rotating bot status & use dynamic variables from the bot!</li>
+                <li><b>💾 sqlite database</b> - Use an `sqlite` database for increased performances!</li>
+                <li><b>🎉 custom embeds</b> - Create your own embeds and use them in your server!</li>
+                <li><b>🎨 customisation</b> - Yep, you heard it right! Even more customisation!</li>
+                <li><b>😁 And so much more...</b></li>
             </ul>
             <div className='flex gap-5 w-1/2 mt-7'>
                 <div className='flex flex-col gap-5 w-1/2'>
